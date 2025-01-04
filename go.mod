34fee819df018ad4fcf0b44fc2b5a4584df5589c
Sebastian Safari <ssebs@ymail.com>
2025-01-03 19:04:33 -0800

    todo

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.5
 
 require (
 	github.com/charmbracelet/bubbletea v1.2.4
-	github.com/charmbracelet/lipgloss v1.0.0
 	github.com/evertras/bubble-table v0.17.1
 	github.com/spf13/pflag v1.0.5
 )
@@ -13,6 +12,7 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbles v0.20.0 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
 	github.com/charmbracelet/x/ansi v0.4.5 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
--- internal/csvmanager.go
+++ internal/csvmanager.go
@@ -7,14 +7,13 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/evertras/bubble-table/table"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type CSVManager struct {
 	FullPath string
 	Contents [][]string
 	Cursor   *CellPosition
-	table    table.Model
 }
 
 // Creates mgr and loads from fullPath
@@ -22,34 +21,12 @@ func NewCSVManager(fullPath string) (mgr *CSVManager, err error) {
 	mgr = &CSVManager{
 		FullPath: fullPath,
 		Cursor:   &CellPosition{0, 0},
-		table:    table.New(nil),
 	}
 
 	err = mgr.Load()
 	if err != nil {
 		return mgr, err
 	}
-
-	cols := make([]table.Column, 0, len(mgr.Contents))
-	rows := make([]table.Row, 0, len(mgr.Contents[0]))
-	for idx, row := range mgr.Contents {
-		// Set columns
-		if idx == 0 {
-			for _, col := range row {
-				cols = append(cols, table.NewFlexColumn(strings.TrimSpace(col), col, 1))
-			}
-			continue
-		}
-
-		// Set rows
-		rowData := table.RowData{}
-		for j, cellData := range row {
-			rowData[cols[j].Key()] = cellData
-		}
-		rows = append(rows, table.NewRow(rowData))
-	}
-
-	mgr.table = mgr.table.WithColumns(cols).WithRows(rows).BorderRounded()
 
 	return mgr, err
 }
@@ -58,8 +35,6 @@ func (mgr *CSVManager) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
-			return mgr, tea.Quit
 		case "up", "k":
 			if mgr.Cursor.Row > 0 {
 				mgr.Cursor.Row--
@@ -76,12 +51,14 @@ func (mgr *CSVManager) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if mgr.Cursor.Col < len(mgr.Contents[0])-1 {
 				mgr.Cursor.Col++
 			}
+		case "ctrl+c":
+			return mgr, tea.Quit
 		case "enter":
 			// edit
-
+			fmt.Println("cursor:", mgr.Cursor)
 		}
 	case tea.WindowSizeMsg:
-		mgr.table.WithTargetWidth(80)
+		fmt.Println("implement resize")
 	}
 
 	return mgr, nil
@@ -90,27 +67,33 @@ func (mgr *CSVManager) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (mgr *CSVManager) View() string {
 	s := strings.Builder{}
 
-	s.WriteString(mgr.table.View())
+	for y := 0; y < len(mgr.Contents); y++ {
+		for x := 0; x < len(mgr.Contents[0]); x++ {
 
-	// for y := 0; y < len(mgr.Contents); y++ {
-	// 	for x := 0; x < len(mgr.Contents[0]); x++ {
-
-	// 		if mgr.Cursor.Col == x && mgr.Cursor.Row == y {
-	// 			tmp := mgr.Contents[y][x]
-	// 			s += lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Render(tmp)
-	// 		} else {
-	// 			s += mgr.Contents[y][x]
-	// 		}
-	// 	}
-	// 	s += "\n"
-	// }
+			cellContent := ""
+			if mgr.Cursor.Col == x && mgr.Cursor.Row == y {
+				// s.WriteString(lipgloss.NewStyle().Border(lipgloss.NormalBorder()).Foreground(lipgloss.Color("12")).Render(cellContent))
+				// s.WriteString(cellContent)
+				cellContent = lipgloss.NewStyle().Padding(1, 1, 1, 1).Render(mgr.Contents[y][x])
+			} else {
+				cellContent = lipgloss.NewStyle().Padding(1, 1, 1, 1).Render(mgr.Contents[y][x])
+			}
+			// TODO: FIGURE OUT WHY ADDING PADDING BREAKS NEWLINES
+			if strings.Contains(cellContent, "\n") {
+				fmt.Println(cellContent)
+			}
+			s.WriteString(cellContent)
+		}
+		s.WriteString("\n")
+	}
 
 	s.WriteString("\nctrl+c to quit")
 	return s.String()
 }
 
 // Set the contents of one cell by a 0 indexed CellPosition
-func (mgr *CSVManager) UpdateCell(pos *CellPosition, value string) error {
+// err if out of bounds
+func (mgr *CSVManager) SetCell(pos *CellPosition, value string) error {
 	if pos.Col < 0 || pos.Row < 0 || pos.Row > len(mgr.Contents) || pos.Col > len(mgr.Contents[0]) {
 		return fmt.Errorf("x/y out of bounds of Contents")
 	}
@@ -119,6 +102,7 @@ func (mgr *CSVManager) UpdateCell(pos *CellPosition, value string) error {
 	return nil
 }
 
+// Save csv mgr.Contents to mgr.FullPath
 func (mgr *CSVManager) Save() error {
 	f, err := os.Create(mgr.FullPath)
 	if err != nil {
@@ -134,6 +118,7 @@ func (mgr *CSVManager) Save() error {
 	return nil
 }
 
+// Load from mgr.FullPath to csv mgr.Contents
 func (mgr *CSVManager) Load() error {
 	f, err := os.Open(mgr.FullPath)
 	if err != nil {
@@ -154,6 +139,7 @@ func (mgr *CSVManager) Init() tea.Cmd {
 	return nil
 }
 
+// 0 indexed
 type CellPosition struct {
 	Row int
 	Col int
